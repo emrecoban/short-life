dd1bf9ee15b32d95cc5d35bdc3be22a1063d349a
emrecoban <fbdarktole@gmail.com>
2023-04-18 21:06:26 +0300

    Added meta tags and header

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
 import { Dongle } from 'next/font/google'
+import Head from 'next/head'
 
 const dongle = Dongle({
   weight: ['300', '700'],
@@ -9,8 +10,14 @@ const dongle = Dongle({
 
 export default function App({ Component, pageProps }) {
   return (
-    <main className={`${dongle.variable} font-sans`}>
-      <Component {...pageProps} />
-    </main>
+    <>
+      <Head>
+        <title>Short Life</title>
+        <link rel="icon" type="image/x-icon" href="img/icon.png" />
+      </Head>
+      <main className={`${dongle.variable} font-sans`}>
+        <Component {...pageProps} />
+      </main>
+    </>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -9,13 +9,20 @@ export async function getStaticProps({ locale }){
   }
 }
 
+export const metaData = {
+  
+}
+
 export default function Home({ languageData }) {
   // Language Settings
   const { locale, locales, defaultLocale } = useRouter()
 
   return (
     <header>
-      
+      <div className="inline-flex items-center max-w-xlbg-slate-400">
+        <img className="h-12 md:h-12 lg:h-16 self-center mx-1" src="img/icon.png" />
+        <h1 className="font-dongle font-bold text-5xl md:text-5xl lg:text-6xl lg:leading-tight text-transparent bg-clip-text bg-gradient-to-r to-[#F66607] from-[#FE8687] underline underline-offset-3 md:decoration-4 lg:decoration-8 decoration-[#DBF0E0]">Short Life</h1>
+      </div>
     </header>
   )
 }
