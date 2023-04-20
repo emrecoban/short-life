76c0e28a6c19910e958bf78ebeec1aeffe2d9db0
emrecoban <fbdarktole@gmail.com>
2023-04-20 21:30:01 +0300

    Added sample life.

--- pages/index.js
+++ pages/index.js
@@ -26,6 +26,274 @@ export default function Home({ languageData }) {
     kareler.push(i);
   }
 
+  const life = {
+    years: [
+      {
+        age: 1,
+        posts: ["hello", "how are you?"]
+      },
+      {
+        age: 2,
+        posts: ["goodbye", "see you later"]
+      },
+      {
+        age: 3,
+        posts: []
+      },
+      {
+        age: 4,
+        posts: []
+      },
+      {
+        age: 5,
+        posts: ["starting preschool", "making new friends"]
+      },
+      {
+        age: 6,
+        posts: ["learning to read", "playing sports"]
+      },
+      {
+        age: 7,
+        posts: ["exploring new hobbies", "making art"]
+      },
+      {
+        age: 8,
+        posts: ["learning to swim", "going on adventures"]
+      },
+      {
+        age: 9,
+        posts: ["making more friends", "building with legos"]
+      },
+      {
+        age: 10,
+        posts: ["learning an instrument", "trying new foods"]
+      },
+      {
+        age: 11,
+        posts: []
+      },
+      {
+        age: 12,
+        posts: []
+      },
+      {
+        age: 13,
+        posts: []
+      },
+      {
+        age: 14,
+        posts: []
+      },
+      {
+        age: 15,
+        posts: []
+      },
+      {
+        age: 16,
+        posts: ["starting to work", "saving for the future"]
+      },
+      {
+        age: 17,
+        posts: ["planning for college", "applying for scholarships"]
+      },
+      {
+        age: 18,
+        posts: ["graduating high school", "starting college"]
+      },
+      {
+        age: 19,
+        posts: ["making career choices", "building a network"]
+      },
+      {
+        age: 20,
+        posts: ["traveling abroad", "exploring new cultures"]
+      },
+      {
+        age: 21,
+        posts: ["celebrating milestones", "trying new things"]
+      },
+      {
+        age: 22,
+        posts: []
+      },
+      {
+        age: 23,
+        posts: []
+      },
+      {
+        age: 24,
+        posts: []
+      },
+      {
+        age: 25,
+        posts: ["celebrating quarter-life", "learning to adult"]
+      },
+      {
+        age: 26,
+        posts: ["planning for the future", "starting a family"]
+      },
+      {
+        age: 27,
+        posts: ["building a home", "investing in the future"]
+      },
+      {
+        age: 28,
+        posts: ["learning to cook", "taking on new challenges"]
+      },
+      {
+        age: 29,
+        posts: ["exploring new hobbies", "making new connections"]
+      },
+      {
+        age: 30,
+        posts: []
+      },
+      {
+        age: 31,
+        posts: []
+        },
+        {
+        age: 32,
+        posts: []
+        },
+        {
+        age: 33,
+        posts: ["finding inner peace", "embracing change"]
+        },
+        {
+        age: 34,
+        posts: ["settling down", "building a family"]
+        },
+        {
+        age: 35,
+        posts: ["balancing work and life", "making time for hobbies"]
+        },
+        {
+        age: 36,
+        posts: []
+        },
+        {
+        age: 37,
+        posts: []
+        },
+        {
+        age: 38,
+        posts: []
+        },
+        {
+        age: 39,
+        posts: ["continuing education", "investing in personal growth"]
+        },
+        {
+        age: 40,
+        posts: []
+        },
+        {
+        age: 41,
+        posts: []
+        },
+        {
+        age: 42,
+        posts: ["finding purpose", "growing spiritually"]
+        },
+        {
+        age: 43,
+        posts: ["finding balance", "prioritizing self-care"]
+        },
+        {
+        age: 44,
+        posts: []
+        },
+        {
+        age: 45,
+        posts: []
+        },
+        {
+        age: 46,
+        posts: ["traveling to new places", "exploring the world"]
+        },
+        {
+        age: 47,
+        posts: []
+        },
+        {
+        age: 48,
+        posts: []
+        },
+        {
+        age: 49,
+        posts: ["building financial security", "planning for retirement"]
+        },
+        {
+        age: 50,
+        posts: ["celebrating golden years", "pursuing passions"]
+        },
+        {
+        age: 51,
+        posts: []
+        },
+        {
+        age: 52,
+        posts: []
+        },
+        {
+        age: 53,
+        posts: []
+        },
+        {
+        age: 54,
+        posts: ["finding joy in everyday moments", "making memories with loved ones"]
+        },
+        {
+        age: 55,
+        posts: ["enjoying retirement", "traveling the world"]
+        },
+        {
+        age: 56,
+        posts: ["spending time with family", "giving back to community"]
+        },
+        {
+        age: 57,
+        posts: ["learning new skills", "embracing change"]
+        },
+        {
+        age: 58,
+        posts: ["discovering new hobbies", "cultivating creativity"]
+        },
+        {
+        age: 59,
+        posts: ["finding meaning in life", "building a legacy"]
+        },
+        {
+        age: 60,
+        posts: ["celebrating milestones", "reflecting on life's journey"]
+        },
+        {
+          age: 61,
+          posts: ["finding joy in everyday moments", "making memories with loved ones"]
+          },
+          {
+          age: 62,
+          posts: ["enjoying retirement", "traveling the world"]
+          },
+          {
+          age: 63,
+          posts: ["spending time with family", "giving back to community"]
+          },
+          {
+          age: 64,
+          posts: ["learning new skills", "embracing change"]
+          },
+          {
+          age: 65,
+          posts: ["discovering new hobbies", "cultivating creativity"]
+          },
+          {
+          age: 66,
+          posts: ["finding meaning in life", "building a legacy"]
+          },
+    ]}  
+
 
   // Language Settings
   const { locale, locales, defaultLocale } = useRouter()
